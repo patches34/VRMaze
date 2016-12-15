66e1f770f289cd6bc1e7bb6eede6966a3bcb506b
Eric Baker <eric.baker@sharpnotions.com>
2016-12-15 13:53:17 -0500

    proof of concept

---
+++ Assets/GridTile.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridTile : MonoBehaviour, IGvrGazeResponder
+{
+	public bool isSafe;
+
+	public Material inactiveMaterial;
+	public Material gazedAtMaterial;
+
+	PlayerMove player;
+
+	void Start() 
+	{
+		SetGazedAt(false);
+
+		player = FindObjectOfType<PlayerMove>();
+	}
+
+	public void SetGazedAt(bool gazedAt) {
+		if (inactiveMaterial != null && gazedAtMaterial != null) {
+			GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+			return;
+		}
+		GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+	}
+
+	public void TeleportHere() {
+		player.WarpTo(transform.position, isSafe);
+	}
+
+	#region IGvrGazeResponder implementation
+
+	/// Called when the user is looking on a GameObject with this script,
+	/// as long as it is set to an appropriate layer (see GvrGaze).
+	public void OnGazeEnter() 
+	{
+		SetGazedAt(true);
+	}
+
+	/// Called when the user stops looking on the GameObject, after OnGazeEnter
+	/// was already called.
+	public void OnGazeExit() 
+	{
+		SetGazedAt(false);
+	}
+
+	/// Called when the viewer's trigger is used, between OnGazeEnter and OnPointerExit.
+	public void OnGazeTrigger() {
+		TeleportHere();
+	}
+
+	#endregion
+}
---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour
+{
+	public float warpTime;
+
+	public Vector3 startPos, resetPos;
+
+	public Rigidbody rBody;
+
+	public float resetTime;
+
+	// Use this for initialization
+	void Start () {
+		resetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void WarpTo(Vector3 pos, bool isSafe)
+	{
+		startPos = transform.position;
+
+		StartCoroutine(WarpLerp(pos, isSafe));
+	}
+
+	IEnumerator WarpLerp(Vector3 targetPos, bool isSafe)
+	{
+		float timer = 0;
+
+		do
+		{
+			timer += Time.deltaTime;
+
+			transform.position = Vector3.Lerp(startPos, targetPos, timer / warpTime);
+
+			if(timer >= warpTime)
+			{
+				break;
+			}
+
+			yield return null;
+		}while(true);
+
+		if(!isSafe)
+		{
+			rBody.useGravity = true;
+
+			StartCoroutine(ResetWait());
+		}
+	}
+
+	IEnumerator ResetWait()
+	{
+		float timer = 0;
+
+		do
+		{
+			timer += Time.deltaTime;
+
+			if(timer >= resetTime)
+				break;
+
+			yield return null;
+		}while(true);
+
+		rBody.useGravity = false;
+		rBody.velocity = Vector3.zero;
+		transform.position = resetPos;
+	}
+}
