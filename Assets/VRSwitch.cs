6430fe11bf130bee80fe3575590529bc1927c1e2
Eric <eabigm@gmail.com>
2018-11-20 15:32:01 -0500

    added Googles code

---
+++ Assets/VRSwitch.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRSwitch : MonoBehaviour
+{
+    [SerializeField]
+    bool isRunningVR;
+
+	// Use this for initialization
+	void Start () {
+        isRunningVR = GvrIntent.IsLaunchedFromVr();
+	}
+
+    void Update()
+    {
+        if (XRSettings.enabled)
+        {
+            // Unity takes care of updating camera transform in VR.
+            return;
+        }
+
+        // android-developers.blogspot.com/2010/09/one-screen-turn-deserves-another.html
+        // developer.android.com/guide/topics/sensors/sensors_overview.html#sensors-coords
+        //
+        //     y                                       x
+        //     |  Gyro upright phone                   |  Gyro landscape left phone
+        //     |                                       |
+        //     |______ x                      y  ______|
+        //     /                                       \
+        //    /                                         \
+        //   z                                           z
+        //
+        //
+        //     y
+        //     |  z   Unity
+        //     | /
+        //     |/_____ x
+        //
+
+        transform.localRotation =
+
+          // Neutral position is phone held upright, not flat on a table.
+          Quaternion.Euler(90f, 0f, 0f) *
+
+          // Sensor reading, assuming default `Input.compensateSensors == true`.
+          Input.gyro.attitude *
+
+          // So image is not upside down.
+          Quaternion.Euler(0f, 0f, 180f);
+    }
+
+    public void ToggleCamera()
+    {
+        if(isRunningVR)
+        {
+            StartCoroutine(SwitchTo2D());
+        }
+        else
+        {
+            StartCoroutine(SwitchToVR());
+        }
+
+        isRunningVR = !isRunningVR;
+    }
+
+    IEnumerator SwitchToVR()
+    {
+        // Device names are lowercase, as returned by `XRSettings.supportedDevices`.
+        string desiredDevice = "cardboard";
+
+        // Some VR Devices do not support reloading when already active, see
+        // https://docs.unity3d.com/ScriptReference/XR.XRSettings.LoadDeviceByName.html
+        if (String.Compare(XRSettings.loadedDeviceName, desiredDevice, true) != 0)
+        {
+            XRSettings.LoadDeviceByName(desiredDevice);
+
+            // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
+            yield return null;
+        }
+
+        // Now it's ok to enable VR mode.
+        XRSettings.enabled = true;
+    }
+
+    IEnumerator SwitchTo2D()
+    {
+        // Empty string loads the "None" device.
+        XRSettings.LoadDeviceByName("");
+
+        // Must wait one frame after calling `XRSettings.LoadDeviceByName()`.
+        yield return null;
+
+        // Restore 2D camera settings.
+        ResetCameras();
+    }
+
+    // Resets camera transform and settings on all enabled eye cameras.
+    void ResetCameras()
+    {
+        // Camera looping logic copied from GvrEditorEmulator.cs
+        for (int i = 0; i < Camera.allCameras.Length; i++)
+        {
+            Camera cam = Camera.allCameras[i];
+            if (cam.enabled && cam.stereoTargetEye != StereoTargetEyeMask.None)
+            {
+                // Reset local position.
+                // Only required if you change the camera's local position while in 2D mode.
+                cam.transform.localPosition = Vector3.zero;
+
+                // Reset local rotation.
+                // Only required if you change the camera's local rotation while in 2D mode.
+                cam.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+}
